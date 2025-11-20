382309ac8ec7da79a97e24d14838558597d7c22d
Ahsan <ahsanm7911@gmail.com>
2025-11-20 17:30:04 +0500

    Reset

--- backend/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path 
-from django.contrib.auth import get_user_model
-from rest_framework import routers, serializers, viewsets
-
-urlpatterns = [
-    
-]
-
--- backend/api/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-
-# Create your views here.
-def test_endpoint(request):
-    return JsonResponse({
-        'user': 'johndoe123', 
-        'gender': 'Male'
-    })
